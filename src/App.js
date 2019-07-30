32d863c970417dcdd264aefcb4dd9888589472ad
Whitney Sam <whitneysamwallace@gmail.com>
2019-07-30 13:27:34 -0700

    adjusted routes for Not Found and Nav

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Nav from './components/Nav';
+import Nav from './components/Nav/Nav';
 import LandingPage from './components/LandingPage/LandingPage';
 import LogIn from './components/LogIn/LogIn';
 import Main from './components/Main/Main';
 import AddStudent from './components/AddStudent/AddStudent';
-import NotFound from './components/NotFound';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Nav className="Nav" />
+      <Nav />
       <Switch>
         <Route exact path="/" component={LandingPage}/>
         <Route path="/login" component={LogIn}/>
