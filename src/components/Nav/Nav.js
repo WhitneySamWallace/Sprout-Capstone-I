2047def8f8e96b73ae1a4806655ab209e3d49447
Whitney Sam <whitneysamwallace@gmail.com>
2019-07-30 14:23:40 -0700

    all tests passing

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LOGIN = true;
 
 function Nav() {
+  const loggedin = (
+    <div className="loggedin">
+      <h1>Sprout</h1>
+      <Link to='/'>Home</Link>
+      <Link to='/'>About</Link>
+      <Link to='/login'>Log In</Link>
+      <Link to='/'>Sign Up</Link>
+    </div>
+  );
+
+  const loggedout = (
+    <div className="loggedout">
+      <h1>Sprout</h1>
+      <Link to='/'>Home</Link>
+      <Link to='/'>Log Out</Link>
+
+    </div>
+  )
   return (
     <div className="Nav">
+      {LOGIN ? loggedin : loggedout}
     </div>
   )
 }
